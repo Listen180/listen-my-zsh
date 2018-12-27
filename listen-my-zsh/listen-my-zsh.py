75c335458bfdaab10fc7eb58fe1456e26f2c1510
listen <listen180@gmail.com>
2018-12-27 17:35:44 +0800

    major update

--- listen-my-zsh/listen-my-zsh.py
+++ listen-my-zsh/listen-my-zsh.py
@@ -24,22 +24,9 @@ OS_TYPE = platform.platform().split('-')[0]
 
 
 @click.command()
-@click.option(
-    'file_type', '--type', '-t',
-    help="Specify the file type. ",
-    required=True,
-#    prompt="Please specify package name",
-)
-@click.argument(
-    'file_dir', 
-    nargs=1, 
-    type=click.Path(exists=True, file_okay=False, writable=True), 
-    required=True, 
-#    default='.',
-)
-def main(file_dir, file_type):
+def oh_my_zsh():
     """
-    Python command line tool to check empty files. 
+    Install oh-my-zsh
     """
     print('')
     print(f'Installing [oh-my-zsh] ...')
@@ -48,6 +35,79 @@ def main(file_dir, file_type):
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 """
     os.system(command_code)
+
+
+
+@click.command()
+def listen_my_zsh():
+    """
+    Install listen-my-zsh (using my customized [.zshrc] file)
+    """
+    print('')
+    print(f'Installing [listen-my-zsh] ...')
+    print('')
+    command_code = """
+if [ -d ~/.listen-my-zsh ]; then
+    read -p "[~/.listen-my-zsh] folder already exists. Are you going to remove the folder and update (Y/n)?" choice
+    case "$choice" in 
+        y|Y )
+            echo "Removing previous folder ... "
+            rm -rf ~/.listen-my-zsh
+            ;;
+        n|N )
+            echo "Aborted"
+            exit
+            ;;
+        * )
+            echo "invalid"
+            exit
+            ;;
+    esac
+fi
+git clone https://github.com/Listen180/listen-my-zsh.git ~/.listen-my-zsh
+echo ""
+echo "... latest [~/.listen-my-zsh] repo downloaded. "
+echo ""
+cp ~/.listen-my-zsh/Ubuntu/.zshrc ~/
+"""
+    os.system(command_code)
+
+
+@click.command()
+def listen_remove():
+    """
+    Revome folder: ~/.listen-my-zsh
+    """
+    command_code = """
+rm -rf ~/listen-my-zsh
+"""
+    os.system(command_code)
+
+
+
+@click.command()
+@click.option(
+    'theme_name', '--theme', '-t',
+    help="Specify the theme name. ",
+    required=False,
+#    prompt="Please specify theme name",
+)
+#@click.argument(
+#    'file_dir', 
+#    nargs=1, 
+#    type=click.Path(exists=True, file_okay=False, writable=True), 
+#    required=True, 
+#)
+def main(theme_name):
+    """
+    Python command line tool to automatically setup fancy zsh environment.  
+    """
+    oh_my_zsh()
+    listen_my_zsh()
+
+
+
+
 
 
 if __name__ == '__main__':
