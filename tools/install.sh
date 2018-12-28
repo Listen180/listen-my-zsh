3c11c7aa282ed0d258912468e74241643556ea85
listen <listen180@gmail.com>
2018-12-28 16:42:53 +0800

    minor update: add logo

--- tools/install.sh
+++ tools/install.sh
@@ -8,6 +8,33 @@
 # * Description   : Install listen-my-zsh
 # *********************************************************
 
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+# Only enable exit-on-error after the non-critical colorization stuff,
+# which may fail on systems lacking tput or terminfo
+set -e
+
+
 ## Install oh-my-zsh
 #if [ ! -d ~/.listen-my-zsh ]; then
 #    echo " Installing oh-my-zsh ... "
@@ -45,29 +72,37 @@ else
 fi
 
 ## Copy the .zshrc file to ~/.zshrc
-if [ -f ~/.zshrc ]; then
-    read -p "[~/.zshrc] file already exists. Are you going to remove it and use the default one in liste-my-zsh? [Y/n]: " choice
-    case "$choice" in 
-        y|Y )
-            echo "Updating ~/.zshrc ... "
-            rm ~/.zshrc
-            cp ~/.listen-my-zsh/Ubuntu/.zshrc ~/
-            ;;
-        n|N )
-            echo "Aborted. "
-            ;;
-        * )
-            echo "Invalid input. (Please choose 'y' or 'n')"
-            ;;
-    esac
+printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+    printf "${YELLOW}Found ~/.zshrc.${NORMAL} ${GREEN}Backing up to ~/.zshrc.pre-listen-my-zsh${NORMAL}\n";
+    mv ~/.zshrc ~/.zshrc.pre-listen-my-zsh;
 fi
+cp ~/.listen-my-zsh/Ubuntu/.zshrc ~/.zshrc
+
+#if [ -f ~/.zshrc ]; then
+#    read -p "[~/.zshrc] file already exists. Are you going to remove it and use the default one in liste-my-zsh? [Y/n]: " choice
+#    case "$choice" in 
+#        y|Y )
+#            echo "Updating ~/.zshrc ... "
+#            rm ~/.zshrc
+#            cp ~/.listen-my-zsh/Ubuntu/.zshrc ~/
+#            ;;
+#        n|N )
+#            echo "Aborted. "
+#            ;;
+#        * )
+#            echo "Invalid input. (Please choose 'y' or 'n')"
+#            ;;
+#    esac
+#fi
+
 
 ## Copy the custom theme into ~/.oh-my-zsh/themes/
 if [ -f ~/.oh-my-zsh/custom/themes/mytheme.zsh-theme ]; then
     rm ~/.oh-my-zsh/custom/themes/mytheme.zsh-theme
 fi
 echo "  copying custom themes into oh-my-zsh custom folder..."
-cp ~/.listen-my-zsh/Ubuntu/mytheme.zsh-theme ~/.oh-my-zsh/custom/themes/
+cp ~/.listen-my-zsh/Ubuntu/mytheme.zsh-theme ~/.oh-my-zsh/custom/themes/mytheme.zsh-theme
 
 ## Make an alias for activatign ~/.zshrc 
 alias actzsh="source ~/.zshrc"
@@ -75,4 +110,14 @@ alias actzsh="source ~/.zshrc"
 #rm -rf ~/.listen-my-zsh
 
 
+
+printf "${GREEN}"
+echo '    __    _      __                                              __   '
+echo '   / /   (_)____/ /____  ____     ____ ___  __  __   ____  _____/ /_  '
+echo '  / /   / / ___/ __/ _ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+echo ' / /___/ (__  ) /_/  __/ / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+echo '/_____/_/____/\__/\___/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+echo '                                         /____/                       '
+echo '                                                                 ... is now installed! '
+printf "${NORMAL}"
 
