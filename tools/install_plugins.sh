d1541da687065fb7717d60592f0751bd13b46bd7
LEI Sen <Listen180@gmail.com>
2019-08-20 11:25:28 +0800

    add new file: install_plugins.sh

---
+++ tools/install_plugins.sh
@@ -0,0 +1,13 @@
+# 
+# *********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2019-08-20 11:24
+# * Last modified : 2019-08-20 11:24
+# * Filename      : install_plugins.sh
+# * Description   : 
+# *********************************************************
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
