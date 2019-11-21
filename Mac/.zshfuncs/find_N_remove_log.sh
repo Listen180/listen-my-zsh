bddf4ecdf04cd87cfc3ac8c219bfb888cef27f75
雷森 <leisen@mininglamp.com>
2019-11-21 17:01:29 +0800

    update

--- Mac/.zshfuncs/find_N_remove_log.sh
+++ Mac/.zshfuncs/find_N_remove_log.sh
@@ -9,7 +9,7 @@
 # *********************************************************
 
 # Clean some log like files of LaTex Compiling
-rm_list=(log aux dvi out synctex.gz nav snm toc)
+rm_list=(log aux dvi out synctex.gz nav snm toc bcf idx lof lot run.xml)
 empty="True"
 for f in "${rm_list[@]}"; do
     res=$(find . -name \*.${f} -type f)
