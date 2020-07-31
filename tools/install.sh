4c5ed8d8f8bda01b222d7ef55193d47e593f78c8
雷森 <leisen@mininglamp.com>
2020-07-31 11:30:57 +0800

    update

--- tools/install.sh
+++ tools/install.sh
@@ -198,8 +198,11 @@ fi
 
 
 ## Add plugins (into oh-my-zsh custom folder)
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+if [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    echo "[${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting] folder already exists. "
+else
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
 echo ''
 printf "${GREEN}"
