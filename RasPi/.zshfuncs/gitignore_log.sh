207daf927a715e7fc6f236e6fde11f845485e3ed
LEI Sen <Listen180@gmail.com>
2021-03-24 19:43:47 +0800

    update: add for RasPi

---
+++ RasPi/.zshfuncs/gitignore_log.sh
@@ -0,0 +1,25 @@
+# 
+# *********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2019-07-17 19:48
+# * Last modified : 2019-10-29 17:02
+# * Filename      : gitignore_log.sh
+# * Description   : 
+# *********************************************************
+
+# Clean some log like files of LaTex Compiling
+ignore_list=(log aux dvi out synctex.gz nav snm toc pdf bcf idx lof lot run.xml)
+for f in "${ignore_list[@]}"; do
+    reg="/*.${f}"
+    if [[ -f ./.gitignore ]]; then
+    check=$(cat ./.gitignore | grep "/\*.${f}")
+    if [[ "${check}" == *"${reg}"* ]];then
+        echo "Already ignored: ${check}"
+    else
+        echo "${reg}" >> ./.gitignore
+    fi
+    else
+    echo "${reg}" > ./.gitignore
+    fi
+done
---
+++ RasPi/.zshfuncs/installListenMyZsh.sh
@@ -0,0 +1,11 @@
+# 
+# *********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2019-10-08 17:26
+# * Last modified : 2019-10-08 17:26
+# * Filename      : installListenMyZsh.sh
+# * Description   : 
+# *********************************************************
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Listen180/listen-my-zsh/master/tools/install.sh)"
