6aefdaffba7488d0816d7bd65a8145e951f4c499
LEI Sen <Listen180@gmail.com>
2022-11-23 10:02:16 +0800

    add auto youtube_dl.sh

---
+++ Mac/.zshfuncs/youtube_dl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# *********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2022-11-23 09:57
+# * Last modified : 2022-11-23 09:57
+# * Filename      : youtube_dl.sh
+# * Description   : 
+# *********************************************************
+
+URL=$1
+RET=1
+until [ ${RET} -eq 0 ]; do
+    youtube-dl -f 137+140 --merge-output-format mp4 ${URL}
+    RET=$?
+    sleep 10
+done
